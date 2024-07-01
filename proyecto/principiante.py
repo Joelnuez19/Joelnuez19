53a2c92f3bb21bd126ae22fdd366a18fc760c43c
Joelnuez19 <148828353+Joelnuez19@users.noreply.github.com>
2024-07-01 07:07:40 -0500

    Create principiante.py

---
+++ proyecto/principiante.py
@@ -0,0 +1,6 @@
+#primer codigo principiante
+
+print("joel nuñez")
+print("1 de julio de 2024")
+print("estoy feliz por programar :)")
+print("""estoy muy contento por estar aprendiendo a programar en python un lenguaje muy facil y versatil de aprender solo ha y que poner mucho empeño y entusiamo""")
