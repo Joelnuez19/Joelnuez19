47fdcaa089535a97a267f9d0a3dadef1f6b92b4e
Joelnuez19 <148828353+Joelnuez19@users.noreply.github.com>
2024-06-30 21:13:34 -0500

    Update app.py

--- proyecto/app.py
+++ proyecto/app.py
@@ -1,6 +1,6 @@
 numero1 = input("introducir un numero: ")
-numero2 = input("introducir el segundo numero: ")
+numero3 = input("introducir el segundo numero: ")
 
-suma = numero1 + numero2
+suma = numero1 + numero3
 
-print(f"la suma de {numero1} + {numero2} es: {suma}")
+print(f"la suma de {numero1} + {numero3} es: {suma}")
